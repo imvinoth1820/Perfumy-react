0716345e8a75c9c6c1f6679dca7c08f5aa7a5d4c
imvinoth1820 <imvinoth1820@gmail.com>
2024-01-23 13:43:45 +0530

    update project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar'
+import Search from './components/Search'
+import Product from './components/Product'
+import About from './components/About'
+import Footer from './components/Footer'
+
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Search />
+            <Product />
+            <About />
+            <Footer />
+        </div>
+    )
+}
+export default App
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+function Search(){
+    return(
+      <div className='searchbar'>
+        <input type="text" placeholder='search'/>
+        </div>
+    )
+  }
+
+  export default Search
